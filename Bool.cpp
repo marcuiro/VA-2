8093ba649109e048bd89a381acbd0713bbbead1d
WarWolfOne <matheussaito95k@gmail.com>
2021-11-03 22:43:39 -0300

    bool

--- Bool.cpp
+++ Bool.cpp
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
 
-bool prioridade;
 
-bool verificarPrioridade();
+
+void verificarPrioridade(bool prioridade, int t, int f);
 
 int main() {
 
+	bool prioridade;
+	int t, f;
 	setlocale(LC_ALL, "Portuguese");
-	
-	printf("Prioridade? \n1 - Sim \n2 - Não\n");
+		
+	printf("Prioridade? \n1 - Sim \n0 - Não\n");
 	scanf("%d", &prioridade);
-	verificarPrioridade();
-	//printf("%s", prioridade?"true":"false");
+
+	verificarPrioridade(prioridade, t, f);
+	
 	return 0;
 
 }
 
 
-bool verificarPrioridade() {
-	if (prioridade = 1) {
-	return true;
-	} else if(prioridade = 0) {
-		return false;
+void verificarPrioridade(bool prioridade, int t, int f) {
+	t = 1;
+	f = 0;
+	if (prioridade == t) {
+		//printf("Good! ");
+		return ;
+	} if(prioridade == f) {
+		//printf("Not good! ");
+		return;
 	}
 }
 
